ce7eeb67a85a8ba719bad4f08557c417c432a2a1
Heasummn <hassamuddin20@gmail.com>
2016-09-18 16:05:00 -0500

    Split boot code into different files

--- kernel/arch/x86_64/boot.s
+++ kernel/arch/x86_64/boot.s
@@ -30,132 +30,12 @@ start:
   call setup_page_tables
   call enable_paging
 
-  lgdt [gdt64.pointer]
+  jmp launch_longmode
   
-  ; Update selectors
-  mov ax, gdt64.data
-  mov ss, ax
-  mov ds, ax
-  mov es, ax
-
-  ; Far jump and start long mode!
-  jmp gdt64.code:longmode_start
-
-
-
-; Make sure the kernel was loaded by a multiboot compliant bootloader
-multiboot_check:
-  ; The specification says that the "multiboot_load" value defined above
-  ;   must be within eax
-  cmp eax, multiboot_load
-  jne .No_Multiboot
-  ret
-
-.No_Multiboot:
-  mov al, "1"
-  jmp error
-
-cpuid_check:
-  ; CPUID is supported if we can invert the ID bit of EFLAGS
-
-  pushfd  ; Push EFLAGS onto stack
-  pop eax ; Copy EFLAGS into eax
-
-  mov ecx, eax ; Copy into ecx to store the old version
-
-  ; Invert the ID bit in eax, but not in EFLAGS
-  xor eax, 0x200000
-
-  ; Copy inverted eax back into EFLAGS
-  push eax
-  popfd
-
-  ; Now, if we restore EFLAGS, and the ID bit is inverted, then CPUID is available
-  ; So let's do exactly that. Store EFLAGS into eax again 
-  pushfd
-  pop eax
-
-  ; Restore the ID bit, by using the copy in ecx
-  ; Doing all this was just a test, we don't want to actually invert the ID bit
-  push ecx
-  popfd
-
-  ; If eax hasn't been changed, then the bit wasn't flipped, and CPUID isn't available
-  cmp eax, ecx
-  jz .No_CPUID
-  ret
-
-.No_CPUID:
-  mov al, "2"
-  jmp error
-
-; Make sure we have longmode available
-longmode_check:
-  mov eax, cpuid_longmode_check ; Set A-register to cpuid longmode check magic
-  cpuid
-  cmp eax, longmode_age       ; CPU is too old if eax is < than longmode_age
-  jb .No_Longmode
-
-  ; If our CPU is old enough to boot into longmode, let's check if we have longmode
-  mov eax, longmode_age
-  cpuid
-  test edx, longmode_bit ; Check if the long mode bit is set
-  jz .No_Longmode
-  ret
-
-.No_Longmode:
-  mov al, "3"
-  jmp error
-
-setup_page_tables:
-  ; Set first P4 entry to P3 table
-  mov eax, p3_table
-  or eax, 3 ; 3 == 0b01 | 0b10. Set read and present
-  mov [p4_table], eax
-
-  ; Set first P3 entry to point to P2 table
-  mov eax, p2_table
-  or eax, 3
-  mov [p3_table], eax
-
-  ; TODO: Map each P2 entry to a page
-  ; To do this, we have to loop!
-
-  mov ecx, 0 ; A counter to count how many pages we've seen
-  .map_page_loop:
-    mov eax, 0x200000 ; 2 MiB
-    mul ecx ; Get the current start address of the page
-    or eax, 0b10000011 ; present + write + huge
-    mov [p2_table + (ecx * 8)], eax ; Since each entry is 8 bits, get our current entry
-
-    ; Loop induction stage
-    inc ecx
-    cmp ecx, 512 ; Each page has 512 entries
-    jne .map_page_loop
-
-    ret
-
-enable_paging:
-  mov eax, p4_table
-  mov cr3, eax  ; For some weird reason, we can't load P4 directly to cr3, so we use a temp
-
-  ; Enable PAE
-  mov eax, cr4
-  or eax, PAE_bit
-  mov cr4, eax
-
-  ; set the long mode bit in the EFER MSR (model specific register)
-  mov ecx, 0xC0000080
-  rdmsr
-  or eax, longmode_bit_efer
-  wrmsr
-
-  ; Actually enable paging
-  mov eax, cr0
-  or eax, paging_bit
-  mov cr0, eax
-
-  ret
+; Include the different stages of booting
+%include "kernel/arch/x86_64/boot_checks.s.inc"
+%include "kernel/arch/x86_64/paging.s.inc"
+%include "kernel/arch/x86_64/gdt.s.inc"
 
 ; Print a nice red on white ERROR: and it's argument
 ; Uses argument from al
@@ -182,17 +62,3 @@ stack_bottom: ; The stack grows downwards, so the bottom is actually at the top
   resb 64
 stack_top:
 
-section .rodata
-gdt64:
-  .empty:
-    dq 0 ; Empty entry
-  .code: equ $ - gdt64
-    ; Code segment
-    dq (1 << descriptor_type) | (1 << present) | (1 << read_write) | (1 << executable) | (1 << code_seg)
-  .data: equ $ - gdt64
-    ; Data segment
-    dq (1 << descriptor_type) | (1 << present) | (1 << read_write)
-  ; Pointer to GDT, fitting the pointer description 
-  .pointer:
-    dw $ - gdt64 - 1
-    dq gdt64
