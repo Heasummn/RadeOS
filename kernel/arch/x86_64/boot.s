05aad210f6b4c6604cd7436fc40fb0565e04c45e
Heasummn <hassamuddin20@gmail.com>
2016-09-18 13:02:28 -0500

    Added error routine

--- kernel/arch/x86_64/boot.s
+++ kernel/arch/x86_64/boot.s
@@ -4,20 +4,23 @@ section 32
 bits 32
 start:
   mov esp, stack_top ; Update the stack pointer
-  ;; We want to print Hello World
-	mov word [0xb8000], 0x0248 ; H
-	mov word [0xb8002], 0x0265 ; e
-  mov word [0xb8004], 0x026c ; l
-  mov word [0xb8006], 0x026c ; l
-  mov word [0xb8008], 0x026f ; o
-	mov word [0xb800a], 0x022c ; ,
-  mov word [0xb800c], 0x0220 ;
-	mov word [0xb800e], 0x0277 ; w
-	mov word [0xb8010], 0x026f ; o
-	mov word [0xb8012], 0x0272 ; r
-	mov word [0xb8014], 0x026c ; l
-  mov word [0xb8016], 0x0264 ; d
-	mov word [0xb8018], 0x0221 ; !
+  
+  mov al, "1"
+  jmp error
+
+  hlt
+
+
+; Print a nice red on white ERROR: and it's argument
+; Uses argument from al
+error:
+  ; 4f == RED on white
+  mov dword [0xb8000], 0x4f524f45 ; 0x45 = E, 0x52 = R
+  mov dword [0xb8004], 0x4f4f4f52 ; 0x52 = R, 0x4f = O
+  mov dword [0xb8008], 0x4f3a4f52 ; 0x52 = R, 0x3a = :
+  mov word  [0xb800c], 0x4f20     ; 0x20 = ' '
+  mov byte  [0xb800e], al         ; Put parameter byte
+  mov byte  [0xb800f], 0x4f
   hlt
 
 section .bss
